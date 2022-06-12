4c0df0e348b9c1c113d70ad7e847565c98564bf5
Anton <confes@bk.ru>
2022-06-13 01:11:32 +0800

    feat(lang): add English language, 'go to top' btn

---
+++ js/translated_en.js
@@ -0,0 +1,104 @@
+var linguJSON = {
+  languages: [
+    {
+      lang_name: "RU",
+      lang_code: "ru",
+      url_pattern: "/",
+    },
+    {
+      lang_name: "EN",
+      lang_code: "en",
+      url_pattern: "?",
+    },
+  ],
+  //   custom_lang_switcher_html:
+  //     '<div id="langswitcher" class="notranslate"><a id="currlanglink" href="">[[linguCurrLang]]</a> <a href="[[linguTargetLangHref1]]" class="targetlang">[[linguTargetLang1]]</a></div>',
+  custom_lang_switcher_container_id: "langlinks",
+  custom_lang_switcher_html:
+    '<span class="notranslate"><a href="?">ENG</a> | <a href="/">RU</a></span>',
+
+  translated_segments: [
+    {
+      source: "Антон Кузьмин",
+      target: "Anton Kuzmin",
+    },
+    {
+      source: "Хобби",
+      target: "Hobbies",
+    },
+    {
+      source: "Опыт",
+      target: "Experience",
+    },
+    {
+      source: "Связаться",
+      target: "Get In Touch",
+    },
+    {
+      source: "Магистр, учитель английского и французского языков.",
+      target: "Master of education. English and French teacher.",
+    },
+    {
+      source:
+        "Фанат своей профессии, временами делаю ремиксы, артворки в Procreate. Изучаю Frontend Development.",
+      target:
+        "I love my job. Remixing & doing artworkd in Procreate. Studying FrontEnd Development",
+    },
+    {
+      source: "Социальные сети",
+      target: "Social Networks",
+    },
+    {
+      source: "Мои хобби",
+      target: "My hobbies",
+    },
+    {
+      source: "Виниловые пластинки",
+      target: "Vinyl records",
+    },
+    {
+      source: "Музыка",
+      target: "Music",
+    },
+    {
+      source: "Авиация",
+      target: "Aviation",
+    },
+    {
+      source: "Опыт работы",
+      target: "Experience",
+    },
+    {
+      source: "СОШ - Россия",
+      target: "Public School - Russia",
+    },
+    {
+      source: "Учитель английского",
+      target: "English Teacher",
+    },
+    {
+      source: "Связаться со мной",
+      target: "Get in touch with me",
+    },
+    {
+      source: "Ваше имя:",
+      target: "Your name:",
+    },
+    {
+      source: "Ваш email:",
+      target: "Your email:",
+    },
+    {
+      source: "Ваше сообщение:",
+      target: "Leave your message here:",
+    },
+    {
+      source: "Введите имя",
+      target: "Your name here",
+    },
+    {
+      source: "О чем бы вы хотели спросить",
+      target: "What would you like to ask me about?",
+    },
+  ],
+};
